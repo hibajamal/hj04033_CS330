4e098ef08eab2306c358c67c86b97e5334f949e9
kk03925 <35625057+kk03925@users.noreply.github.com>
2019-04-26 18:16:08 +0500

    Update MEM_WB.v

--- PROJECT/MEM_WB.v
+++ PROJECT/MEM_WB.v
@@ -8,18 +8,18 @@ module MEM_WB():
     input [63:0] Read_Data,
     output reg [63:0] Read_Data_out,
     
-    input [4:0] EX_MEM/Rd,
-    output reg [4:0] EX_MEM/Rd_out,
+    input [4:0] EX_MEM_Rd,
+    output reg [4:0] EX_MEM_Rd_out,
 
-    input [63:0] EX_MEM/Mem_Address,
-    output reg [63:0] EX_MEM/Mem_Address_out,
+    input [63:0] EX_MEM_Mem_Address,
+    output reg [63:0] EX_MEM_Mem_Address_out,
 
 );
 
     always@(posedge clk)
     begin
       {Mem_to_Reg, Reg_Write}= WB;
-      EX_MEM/MEM_Address = EX_MEM/Mem_Address_out;
+      EX_MEM_MEM_Address = EX_MEM_Mem_Address_out;
       Read_Data_out = Read_Data;
       EX_MEM.Rd_out = EX_MEM.Rd;
     end
@@ -28,9 +28,9 @@ module MEM_WB():
     begin
       Mem_to_Reg = 1'b0;
       Reg_Write = 1'b0;
-      EX_MEM/Mem_Address_out = 64'b0;
+      EX_MEM_Mem_Address_out = 64'b0;
       Read_Data_out = 64'b0;
-      EX_MEM/Rd_out = 5'b0;
+      EX_MEM_Rd_out = 5'b0;
     end
     
 endmodule
