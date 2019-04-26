9f3dbb9de6fe80dfa488e0b5fa5c76c3cdd8b6c5
kk03925 <35625057+kk03925@users.noreply.github.com>
2019-04-26 11:59:21 +0500

    Add files via upload

--- PROJECT/Forwarding_Unit.v
+++ PROJECT/Forwarding_Unit.v
@@ -7,6 +7,26 @@ module Forwarding_Unit
   input EX_MEM_RegWrite,
   input MEM_WB_RegWrite,
   
-  outpuy
-  
-);
+  output reg [1:0] ForwardA,
+  output reg [1:0] ForwardB,
+); 	
+
+	always@( ID_EX_RS1 or ID_EX_RS2 or EX_MEM_Rd or MEM_WB_Rd or EX_MEM_RegWrite or MEM_WB_RegWrite)
+	begin
+		if(EX_MEM_RegWrite and (EX_MEM_Rd != 5'b0) and (EX_MEM_Rd == ID_EX_RS1))
+		begin
+			ForwardA = 2'b10
+		end
+		if(MEM_WB_RegWrite and (MEM_WB_Rd != 5'b0) and (MEM_WB_Rd == ID_EX_RS1) and ~(EX_MEM_RegWrite and (EX_MEM_Rd != 0)) and (EX_MEM_Rd = ID_EX_RS1))
+		begin
+			ForwardA = 2'b01
+		end
+		if(EX_MEM_RegWrite and (EX_MEM_Rd != 5'b0) and (EX_MEM_Rd == ID_EX_RS2))
+		begin
+			ForwardB = 2'b10
+		end
+		if(MEM_WB_RegWrite and (MEM_WB_Rd != 5'b0) and (MEM_WB_Rd == ID_EX_RS2) and ~(EX_MEM_RegWrite and (EX_MEM_Rd != 0)) and (EX_MEM_Rd = ID_EX_RS2))
+		begin
+			ForwardB = 2'b01
+		end
+	end
