1cfd1c626df946ce1269d355c57fd4aae74303cc
hibajamal <35984866+hibajamal@users.noreply.github.com>
2019-04-26 12:55:59 +0500

    Update Forwarding_Unit.v

--- PROJECT/Forwarding_Unit.v
+++ PROJECT/Forwarding_Unit.v
@@ -8,25 +8,25 @@ module Forwarding_Unit
   input MEM_WB_RegWrite,
   
   output reg [1:0] ForwardA,
-  output reg [1:0] ForwardB,
+  output reg [1:0] ForwardB
 ); 	
 
 	always@(*)
 	begin
 		if(EX_MEM_RegWrite and (EX_MEM_Rd != 5'b0) and (EX_MEM_Rd == ID_EX_RS1))
 		begin
-			ForwardA = 2'b10
+			ForwardA = 2'b10;
 		end
 		if(MEM_WB_RegWrite and (MEM_WB_Rd != 5'b0) and (MEM_WB_Rd == ID_EX_RS1) and ~(EX_MEM_RegWrite and (EX_MEM_Rd != 0)) and (EX_MEM_Rd = ID_EX_RS1))
 		begin
-			ForwardA = 2'b01
+			ForwardA = 2'b01;
 		end
 		if(EX_MEM_RegWrite and (EX_MEM_Rd != 5'b0) and (EX_MEM_Rd == ID_EX_RS2))
 		begin
-			ForwardB = 2'b10
+			ForwardB = 2'b10;
 		end
 		if(MEM_WB_RegWrite and (MEM_WB_Rd != 5'b0) and (MEM_WB_Rd == ID_EX_RS2) and ~(EX_MEM_RegWrite and (EX_MEM_Rd != 0)) and (EX_MEM_Rd = ID_EX_RS2))
 		begin
-			ForwardB = 2'b01
+			ForwardB = 2'b01;
 		end
 	end
