8203d3c34a973f314058c356fb128cca9aa5b76e
hibajamal <35984866+hibajamal@users.noreply.github.com>
2019-04-26 12:55:36 +0500

    Update Control_Unit.v

--- PROJECT/Control_Unit.v
+++ PROJECT/Control_Unit.v
@@ -4,12 +4,10 @@ module Control_Unit
 	
 	output reg [2:0] EX,
 	output reg [2:0] M,	
-	output reg [1:0] WB,
+	output reg [1:0] WB
 );
 
 	always@(*)
 	begin
-		WB = Opcode[1:0]
-		M  = Opcode[4:2]
-		EX = Opcode[6:4]
+		{WB, M, EX} = Opcode;
 	end
