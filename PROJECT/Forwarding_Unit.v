9fad5e9a152d1d790ebe8d3ab87c6051f1f689b8
hibajamal <35984866+hibajamal@users.noreply.github.com>
2019-04-26 12:52:07 +0500

    Update Forwarding_Unit.v

--- PROJECT/Forwarding_Unit.v
+++ PROJECT/Forwarding_Unit.v
@@ -11,7 +11,7 @@ module Forwarding_Unit
   output reg [1:0] ForwardB,
 ); 	
 
-	always@( ID_EX_RS1 or ID_EX_RS2 or EX_MEM_Rd or MEM_WB_Rd or EX_MEM_RegWrite or MEM_WB_RegWrite)
+	always@(*)
 	begin
 		if(EX_MEM_RegWrite and (EX_MEM_Rd != 5'b0) and (EX_MEM_Rd == ID_EX_RS1))
 		begin
